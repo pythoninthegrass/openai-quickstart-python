e2ada6206affa84dc364e39acf0fa04a5eadfc0e
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2023-01-14 23:42:37 -0600

    init

--- README.md
+++ README.md
@@ -41,4 +41,4 @@ This is an example pet name generator app used in the OpenAI API [quickstart tut
    $ flask run
    ```
 
-You should now be able to access the app at [http://localhost:5000](http://localhost:5000)! For the full context behind this example app, check out the [tutorial](https://beta.openai.com/docs/quickstart).
+You should now be able to access the app at [http://localhost:8000](http://localhost:8000)! For the full context behind this example app, check out the [tutorial](https://beta.openai.com/docs/quickstart).
--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-import os
+#!/usr/bin/env python3
 
 import openai
+import os
+from decouple import config
 from flask import Flask, redirect, render_template, request, url_for
 
-app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# custom app name based on tld w/host, port, debug options
+app = Flask("openai-quickstart-python")                     # default (__name__)
+
+if config("OPENAI_API_KEY") is not None:
+    openai.api_key = config("OPENAI_API_KEY")
+else:
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 @app.route("/", methods=("GET", "POST"))
@@ -23,13 +31,17 @@ def index():
 
 
 def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+    return """Suggest ten names for an animal that is a superhero.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+    Animal: Cat
+    Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+    Animal: Dog
+    Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+    Animal: {}
+    Names:""".format(
+            animal.capitalize()
+        )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
